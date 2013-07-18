bf131c7a2bd351d720044a6ab24ceb4b56802b31
espec94 <seungkyun.hong@gmail.com>
2013-07-19 00:03:39 +0100

    remove...

--- assets/www/WEB-INF/classes/com/googlecode/mgwt/examples/showcase/client/common/ApplicationConstants.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.mgwt.examples.showcase.client.common;
-
-public class ApplicationConstants {
-
-    public static String BASE_URL = "http://api.irishrail.ie/realtime/realtime.asmx";
-
-    public static String ALL_STATIONS = "getAllStationsXML";
-}
